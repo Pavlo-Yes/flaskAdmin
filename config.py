3fd7018efc1e6f024860c201971b0184d58cca97
demchyshynv <K1l@t1V1>
2020-09-22 21:32:55 +0300

    Flask-Admin

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    DEBUG = False
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@localhost/admin_db'
+    SECRET_KEY = 'very secret key'
